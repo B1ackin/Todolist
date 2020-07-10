4bfcb7ab43130562d5d2ea1b4e3fef040f8d9114
B1ackin <xx13.02@gmail.com>
2020-07-10 21:11:30 +0300

    Updates

---
+++ precache-manifest.346da46fda3de26deac57effe921eb48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e83dc5c302a1502e90aa3c69f1c2320",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "56dc6d22ac0a64d536ea",
+    "url": "/todolist/static/css/main.06027f3b.chunk.css"
+  },
+  {
+    "revision": "00b3fd595ccc0ecd0435",
+    "url": "/todolist/static/js/2.3f6dae53.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist/static/js/2.3f6dae53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56dc6d22ac0a64d536ea",
+    "url": "/todolist/static/js/main.0fd73bb0.chunk.js"
+  },
+  {
+    "revision": "600da8c785242bbca43d",
+    "url": "/todolist/static/js/runtime-main.d803f98f.js"
+  }
+]);
